fa4dd42af7f6f9f0d87d59821c58720596c10623
Truong Trong Hai <truongtronghai@gmail.com>
2024-04-06 19:51:53 +0700

    Revert "remove primevue and tailwindcss"
    This reverts commit f9f6babcd24af21144d915fa08d53e1c7b4cf802.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,23 @@
 import path from 'path'
 
 export default defineNuxtConfig({
-	modules: ['@pinia/nuxt'],
-	css: ['~/assets/css/main.css'],
+	modules: ['nuxt-primevue', '@pinia/nuxt'],
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
+	css: ['~/assets/css/main.css', 'primeicons/primeicons.css'],
+	primevue: {
+		options: {
+			unstyled: true,
+		},
+		importPT: { from: path.resolve(__dirname, './assets/presets/lara/') },
+		components: {
+			exclude: ['Editor', 'Chart'],
+		},
+	},
 	pinia: {
 		storesDirs: ['./stores/**'],
 	},
